481d607d25bf84e61692f87e827f99a2c49d32d7
shahab-qazavi <shahab.geek@gmail.com>
2020-08-24 18:10:49 +0430

    change port build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker stop taskmanager
 docker rm taskmanager
 docker rmi taskmanager
 docker build -t taskmanager .
-docker run -d --restart always --name taskmanager -p 8011:8000 --network mongo taskmanager
+docker run -d --restart always --name taskmanager -p 8012:7575 --network mongo taskmanager
